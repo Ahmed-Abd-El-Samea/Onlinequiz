73104d3d4dd6cc669f11010486c08493f64a8fd7
ahmed <ahmedabdo6868@yahoo.com>
2016-08-23 10:59:34 +0200

    first commit

---
+++ quiz/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+class Answers(models.Model):
+	phone_num = models.BigIntegerField()
+	answer1 = models.TextField()
+	answer2 = models.TextField()
+	answer3 = models.TextField()
+	answer4 = models.TextField()
+	answer5 = models.TextField()
+	answer6 = models.TextField()
+	answer7 = models.TextField()
+	answer8 = models.TextField()
+	answer9 = models.TextField()
+	answer10 = models.TextField()
